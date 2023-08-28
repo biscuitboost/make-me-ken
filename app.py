b227843e00d84a1afe0e9e63e2d8584de31b87b7
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-08-28 15:58:56 +0100

    adding cropping

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import io
 from PIL import Image
 import replicate
+from streamlit_cropper import st_cropper
 
 # Set up configurations
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -13,7 +14,7 @@ DESC = '''Upload or take a picture to become Ken or Barbie'''
 
 def run_model(target_image_path, swap_image_data):
     output = replicate.run(
-        "lucataco/faceswap:9a4298548422074c3f57258c5d544497314ae4112df80d116f0d2109e843d20d",
+        MODEL_ADDRESS,
         input={
             "target_image": open(target_image_path, "rb"),
             "swap_image": io.BytesIO(swap_image_data)
@@ -29,8 +30,20 @@ def main():
     image_file = st.file_uploader("Upload Clear Photo Of Your Face.", type=['jpg', 'png'])
 
     if image_file is not None:
-        image_data = image_file.read()
-        st.image(image_data, width = 45)
+        img = Image.open(image_file)
+        realtime_update = st.checkbox(label="Crop in Real Time", value=True)
+
+        if realtime_update:
+            st.write("Double click to save crop")
+
+        # Get a cropped image from the frontend
+        cropped_img = st_cropper(img, realtime_update=realtime_update, box_color="#0000FF",
+                                aspect_ratio=(1, 1))
+
+        st.image(cropped_img, width = 512)
+        buf = io.BytesIO()
+        cropped_img.save(buf, format='JPEG')
+        byte_im = buf.getvalue()
 
         if st.button("Make Me Ken"):
             if target_image_option == 'Ken':
@@ -38,8 +51,8 @@ def main():
             else:
                 target_image_path = 'barbie.jpg'
 
-            output = run_model(target_image_path, image_data)
-            st.image(output, width = 725)
+            output = run_model(target_image_path, byte_im)
+            st.image(output["result"], use_column_width=True)
 
 if __name__ == '__main__':
   main()
