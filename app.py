5fed4e242e33699d65ef482b5bc64c3ab42a199f
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-08-29 11:16:44 +0100

    dd

--- app.py
+++ app.py
@@ -2,16 +2,41 @@ import streamlit as st
 import requests
 import io
 from PIL import Image
+from psd_tools import PSDImage
 import replicate
 from streamlit_cropper import st_cropper
 
 # Set up configurations
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
+################
+# Global variables
+################
 MODEL_ADDRESS = "lucataco/faceswap:9a4298548422074c3f57258c5d544497314ae4112df80d116f0d2109e843d20d"
 TITLE = "I want to be Ken"
 DESC = '''Upload or take a picture to become Ken or Barbie'''
 
+################
+# Update PSD with smart objects
+# Inputs: PSD file, input file, smart object
+# Outputs: new updated psd
+################
+def update_psd(psd_file, input_files, smartobject):
+    with PSDImage.open(psd_file) as psd:
+        for layer in psd.layers:
+            if layer.name == smartobject:
+                # Get the embedded smart object path
+                embedded_path = layer.smart_object.file.reference.filename
+                # Check if the embedded smart object is a psd file
+                if embedded_path in input_files:
+                    # Replace the embedded smart object with the input file
+                    layer.smart_object.file = input_files[embedded_path]
+        # save the updated psd file
+        psd.save('back-to-future2.psd')
+    
+################
+# Swap Face Model
+################
 def run_model(target_image_path, swap_image_data):
     output = replicate.run(
         MODEL_ADDRESS,
@@ -21,7 +46,10 @@ def run_model(target_image_path, swap_image_data):
         }
     )
     return output
-
+    
+################
+# Main function
+################
 def main():
     st.sidebar.title(TITLE)
     st.sidebar.write(DESC)
@@ -55,8 +83,10 @@ def main():
                 target_image_path = 'ken.jpg'
             else:
                 target_image_path = 'barbie.jpg'
-            output = run_model(target_image_path, byte_im)
-
+            #output = run_model(target_image_path, byte_im)
+            output = "ken.jpg"
+            update_psd('back-to-future.psd', byte_im, '-e-doc')
+            
             # Replacing the cropped image with the output image from the model
             with col2:
                 st.header("Output Image")
