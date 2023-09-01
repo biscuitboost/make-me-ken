870ec6741ed205d98a211b7fcf9265cb3554aa0e
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-09-01 13:57:32 +0100

    kkk

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import io
 from PIL import Image
 from psd_tools import PSDImage
 import replicate
+import matplotlib.pyplot as plt
 
 # Set up configurations
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -12,7 +13,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 ################
 MODEL_ADDRESS = "lucataco/faceswap:9a4298548422074c3f57258c5d544497314ae4112df80d116f0d2109e843d20d"
 TITLE = "I want to be Ken"
-DESC = '''Upload or take a picture to become Ken or Barbie'''
+DESC = "Upload or take a picture to become Ken or Barbie"
 
 ################
 # Update PSD with smart objects
@@ -21,32 +22,30 @@ DESC = '''Upload or take a picture to become Ken or Barbie'''
 ################
 def update_psd(psd_file, input_files, smartobject):
     st.write("psd_file = ", psd_file)
-    #st.write("input_files = ", input_files)
+    # st.write("input_files = ", input_files)
     st.write("smartobject = ", smartobject)
     psd = PSDImage.open(psd_file)
-    psd.composite().save('example.png')
+    psd.composite().save("example.png")
 
-    
     for layer in psd:
         layer_name = layer.name
         if layer_name == smartobject:
             st.write("Found smart object")
-            #layer.replace_contents(input_files)
-            #st.write("Replaced smart object")
-        #layer_image = layer.composite()
-        #layer_image.save('%s.png' % layer.name)
-    
-    # Find the smart object layer
-    #layer = psd.smart_object_layers[smartobject]
-    
-    # Replace the contents of the smart object layer with the new image
-    #layer.replace_contents(input_files[layer.filepath])
-    
-    # Save the updated PSD file
-    #psd.save()
+            # layer.replace_contents(input_files)
+            # st.write("Replaced smart object")
+        # layer_image = layer.composite()
+        # layer_image.save('%s.png' % layer.name)
 
-        
-    
+    # Find the smart object layer
+    # layer = psd.smart_object_layers[smartobject]
+
+    # Replace the contents of the smart object layer with the new image
+    # layer.replace_contents(input_files[layer.filepath])
+
+    # Save the updated PSD file
+    # psd.save()
+
+
 ################
 # Swap Face Model
 ################
@@ -55,11 +54,12 @@ def run_model(target_image_path, swap_image_data):
         MODEL_ADDRESS,
         input={
             "target_image": open(target_image_path, "rb"),
-            "swap_image": io.BytesIO(swap_image_data)
-        }
+            "swap_image": io.BytesIO(swap_image_data),
+        },
     )
     return output
-    
+
+
 ################
 # Main function
 ################
@@ -67,8 +67,10 @@ def main():
     st.sidebar.title(TITLE)
     st.sidebar.write(DESC)
 
-    target_image_option = st.sidebar.radio('Take Your Pick:', ('Ken', 'Barbie'))
-    image_file = st.sidebar.file_uploader("Upload Clear Photo Of Your Face.", type=['jpg', 'png'])
+    target_image_option = st.sidebar.radio("Take Your Pick:", ("Ken", "Barbie"))
+    image_file = st.sidebar.file_uploader(
+        "Upload Clear Photo Of Your Face.", type=["jpg", "png"]
+    )
 
     if image_file is not None:
         img = Image.open(image_file)
@@ -76,23 +78,29 @@ def main():
 
         # Check if the image is valid and not empty
         if len(byte_im) > 0:
-            make_ken_button = st.sidebar.button("Make Me Ken", key='make_ken_button')
+            make_ken_button = st.sidebar.button(
+                "Make Me Ken", key="make_ken_button"
+            )
             if make_ken_button:
-                if target_image_option == 'Ken':
-                    target_image_path = 'ken.jpg'
+                if target_image_option == "Ken":
+                    target_image_path = "ken.jpg"
                 else:
-                    target_image_path = 'barbie.jpg'
+                    target_image_path = "barbie.jpg"
                 output = run_model(target_image_path, byte_im)
+
                 if output is not None:
                     # Output image is not None
                     st.header("Output Image")
-                    st.image(output, width=256)
+                    plt.imshow(output)
+                    plt.axis("off")
+                    st.pyplot()
                     st.balloons()
                 else:
                     # Output image is None
                     st.error("Error: Failed to generate the output image.")
                     st.warning("Please try again with a different image.")
                     st.sidebar.empty()
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
