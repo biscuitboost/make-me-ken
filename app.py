2d1754905ca9f3d8e8a6bf4004b58163cc711182
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-09-01 12:06:40 +0100

    sss

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import io
 from PIL import Image
 from psd_tools import PSDImage
 import replicate
-from streamlit_cropper import st_cropper
 
 # Set up configurations
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -22,30 +21,32 @@ DESC = '''Upload or take a picture to become Ken or Barbie'''
 ################
 def update_psd(psd_file, input_files, smartobject):
     st.write("psd_file = ", psd_file)
-    # st.write("input_files = ", input_files)
+    #st.write("input_files = ", input_files)
     st.write("smartobject = ", smartobject)
     psd = PSDImage.open(psd_file)
     psd.composite().save('example.png')
 
+    
     for layer in psd:
         layer_name = layer.name
         if layer_name == smartobject:
             st.write("Found smart object")
-            # layer.replace_contents(input_files)
-            # st.write("Replaced smart object")
-        # layer_image = layer.composite()
-        # layer_image.save('%s.png' % layer.name)
-
+            #layer.replace_contents(input_files)
+            #st.write("Replaced smart object")
+        #layer_image = layer.composite()
+        #layer_image.save('%s.png' % layer.name)
+    
     # Find the smart object layer
-    # layer = psd.smart_object_layers[smartobject]
-
+    #layer = psd.smart_object_layers[smartobject]
+    
     # Replace the contents of the smart object layer with the new image
-    # layer.replace_contents(input_files[layer.filepath])
-
+    #layer.replace_contents(input_files[layer.filepath])
+    
     # Save the updated PSD file
-    # psd.save()
+    #psd.save()
 
-
+        
+    
 ################
 # Swap Face Model
 ################
@@ -54,12 +55,11 @@ def run_model(target_image_path, swap_image_data):
         MODEL_ADDRESS,
         input={
             "target_image": open(target_image_path, "rb"),
-            "swap_image": io.BytesIO(swap_image_data),
-        },
+            "swap_image": io.BytesIO(swap_image_data)
+        }
     )
     return output
-
-
+    
 ################
 # Main function
 ################
@@ -67,51 +67,28 @@ def main():
     st.sidebar.title(TITLE)
     st.sidebar.write(DESC)
 
-    target_image_option = st.sidebar.radio("Take Your Pick:", ("Ken", "Barbie"))
-    image_file = st.sidebar.file_uploader(
-        "Upload Clear Photo Of Your Face.", type=["jpg", "png"]
-    )
+    target_image_option = st.sidebar.radio('Take Your Pick:', ('Ken', 'Barbie'))
+    image_file = st.sidebar.file_uploader("Upload Clear Photo Of Your Face.", type=['jpg', 'png'])
 
     if image_file is not None:
         img = Image.open(image_file)
-        width, height = img.size
-        aspect_ratio = float(width) / height
-        aspect_ratio = (aspect_ratio, 1)  # Convert to tuple
-        realtime_update = True
+        byte_im = img.tobytes()
 
-        # Adjust the image display size based on aspect ratio
-        img_width = min(640, int(aspect_ratio[0] * 640))
-        img_height = int(img_width / aspect_ratio[0])
+        # Check if the image is valid and not empty
+        if len(byte_im) > 0:
+            make_ken_button = st.sidebar.button("Make Me Ken", key='make_ken_button')
+            if make_ken_button:
+                if target_image_option == 'Ken':
+                    target_image_path = 'ken.jpg'
+                else:
+                    target_image_path = 'barbie.jpg'
+                output = run_model(target_image_path, byte_im)
+                #output = "ken.jpg"
+                #update_psd('back-to-future.psd', byte_im, '-e-doc')
+                # Replacing the input image with the output image from the model
+                st.header("Output Image")
+                st.image(output, width=256)
+                st.balloons()
 
-        # Move the input image to a separate column
-        col1, col2 = st.columns([2, 1])
-        with col1:
-            cropped_img = st_cropper(
-                img, realtime_update=realtime_update, box_color="#e0218a", aspect_ratio=aspect_ratio
-            )
-            # Resize the image
-            cropped_img = cropped_img.resize((img_width, img_height))
-            st.image(cropped_img, width=img_width)
-            buf = io.BytesIO()
-            cropped_img.save(buf, format="JPEG")
-            byte_im = buf.getvalue()
-
-        with col2:
-            if byte_im is not None:
-                # Move "Make me Ken" button to the sidebar and disable it if byte_im is None
-                make_ken_button = st.sidebar.button(
-                    "Make Me Ken", key="make_ken_button", disabled=(byte_im is None)
-                )
-                if make_ken_button:
-                    if target_image_option == "Ken":
-                        target_image_path = "ken.jpg"
-                    else:
-                        target_image_path = "barbie.jpg"
-                    output = run_model(target_image_path, byte_im)
-                    st.header("Output Image")
-                    st.image(output, width=256)
-                    st.balloons()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
