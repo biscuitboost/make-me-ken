736b780e77e6c64153d8244d13893d4be7959914
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-09-01 15:04:02 +0100

    sss

--- app.py
+++ app.py
@@ -86,6 +86,9 @@ def main():
                 else:
                     target_image_path = 'images/barbie.jpg'
                 output = run_model(target_image_path, image_data)
+                # save output image url to local
+                with open("output.jpg", "wb") as f:
+                    f.write(output)
                 add_text_to_image(output, "I'm Ken!")
                 #output = "ken.jpg"
                 # Replacing the input image with the output image from the model
